30850869e300892f99740eac154210b8e3fc224e
HR0814 <yoohlee1@naver.com>
2021-06-09 11:38:33 +0900

    practice

--- src/practice2/p10.java
+++ src/practice2/p10.java
@@ -1,9 +1,21 @@
 package practice2;
+import java.util.Scanner;
 
 public class p10 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
+		int add, subtract, divide, multiply = 0;
+		
+		add = num1 + num2;
+		subtract = num1 - num2;
+		divide = num1 / num2;
+		multiply = num1 * num2;
+		
+		System.out.println(add);
 	}
 
 }
--- src/practice2/p9_1.java
+++ src/practice2/p9_1.java
@@ -1,13 +1,25 @@
 package practice2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class p9_1 {
 
 	public static void main(String[] args) {
+
+		int data1[] = {80,90,95,75,70};
+		int data2[] = {90,85,85,85,80};
+		int sum_a = 0;
+		int sum_b = 0;
 		
-		Scanner sc = new Scanner(System.in);
-		
+		for(int i=0;i<5;i++) {
+			sum_a += data1[i];
+			sum_b += data2[i];
+		}
+		System.out.println("A: " + Arrays.toString(data1));
+		System.out.println("Sum(A) = " + sum_a);
+		System.out.println("B: " + Arrays.toString(data2));
+		System.out.println("Sum(B) = " + sum_b);
 		
 	}
 
