4157030d3b7a2255bdc389e5a83309a2f52fb6f7
HR0814 <yoohlee1@naver.com>
2021-06-14 11:38:24 +0900

    practice

--- src/practice2/p10.java
+++ src/practice2/p10.java
@@ -3,19 +3,50 @@ import java.util.Scanner;
 
 public class p10 {
 
+	public static double add(int x, int y) {
+		return x + y;
+	}
+	public static double subtract(int x, int y) {
+		return x - y;
+	}
+	public static double divide(int x, int y) {
+		return x / y;
+	}
+	public static double multiply(int x, int y) {
+		if (y == 0) {
+			System.out.println("0으로 나눌 수 없습니다.");
+			return 0;
+		}
+		else return x * y;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
 		
-		int add, subtract, divide, multiply = 0;
+		int a = sc.nextInt();
+		char ch = sc.next().charAt(0);
+		int b = sc.nextInt();
+		double result = 0;
 		
-		add = num1 + num2;
-		subtract = num1 - num2;
-		divide = num1 / num2;
-		multiply = num1 * num2;
-		
-		System.out.println(add);
+		if(ch == '+')
+		{
+			result = add(a,b);
+		}
+		else if (ch == '-')
+		{
+			result = subtract(a,b);
+		}
+		else if (ch == '/')
+		{
+			result = divide(a,b);
+		}
+		else
+		{
+			result = multiply(a,b);
+		}
+							
+		System.out.println(" = "+result);
+						
 	}
 
 }
---
+++ src/practice2/p11.java
@@ -0,0 +1,9 @@
+package practice2;
+
+public class p11 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
